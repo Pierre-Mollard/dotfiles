f10f12f7e90a8c43527e953b1b2814a6ae6715b1
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-01-02 00:28:33 +0100

    feat: added transparent bg for tmux and nvim

--- dotfiles/nvim/config/nvim/lua/plugins/catppuccin.lua
+++ dotfiles/nvim/config/nvim/lua/plugins/catppuccin.lua
@@ -1,4 +1,37 @@
 return {
+  -- Configure catppuccin to be transparent
+  {
+    "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = { enabled = true },
+        neotest = true,
+        neotree = true, -- File explorer transparency
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
   -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
