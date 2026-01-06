099e9bf13dcef7a7e860070c112d91a878802a0d
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-01-06 20:48:54 +0100

    feat: added ptuscript tree-sitter auto fold support

--- dotfiles/nvim/config/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/config/nvim/lua/config/autocmds.lua
@@ -32,6 +32,7 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.treesitter.start()
     vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.wo[0][0].foldmethod = 'expr'
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indent.get_indent()"
   end,
 })
 
--- dotfiles/nvim/config/nvim/lua/plugins/tree-sitter-ptuscript.lua
+++ dotfiles/nvim/config/nvim/lua/plugins/tree-sitter-ptuscript.lua
@@ -9,6 +9,10 @@ return {
       vim.filetype.add({ extension = { ptu = "ptuscript" } })
       vim.treesitter.language.add('ptuscript', { path = "/home/pierre/projects/tree-sitter-ptuscript/ptuscript.so" })
       vim.treesitter.language.register('ptuscript', { 'ptu' })
-    end,
-  }
-}
+      vim.opt_local.foldmethod = "expr"
+      vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.opt_local.foldlevel = 99
+      vim.opt_local.indentexpr = "v:lua.require'nvim-treesitter'.indent.get_indent()"
+      end,
+    }
+ }
