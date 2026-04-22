b60148712a35d828c319b649c03eca282ccaf026
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-04-22 23:05:05 +0200

    fix: maj vim, fix treesitter, fix avante list

--- stow/nvim/.config/nvim/lua/plugins/tree-sitter-ptuscript.lua
+++ stow/nvim/.config/nvim/lua/plugins/tree-sitter-ptuscript.lua
@@ -4,15 +4,14 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "typescript",
-        "ptuscript"
       })
       vim.filetype.add({ extension = { ptu = "ptuscript" } })
-      vim.treesitter.language.add('ptuscript', { path = "/home/pierre/projects/tree-sitter-ptuscript/ptuscript.so" })
-      vim.treesitter.language.register('ptuscript', { 'ptu' })
+      vim.treesitter.language.add("ptuscript", { path = "/home/pierre/projects/tree-sitter-ptuscript/ptuscript.so" })
+      vim.treesitter.language.register("ptuscript", { "ptu" })
       vim.opt_local.foldmethod = "expr"
       vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
       vim.opt_local.foldlevel = 99
       vim.opt_local.indentexpr = "v:lua.require'nvim-treesitter'.indent.get_indent()"
-      end,
-    }
- }
+    end,
+  },
+}
