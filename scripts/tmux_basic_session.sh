2b834d8f8cbf1702bf04020bd0d8fc5a01c10ed1
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-04-22 18:51:21 +0200

    fix: bad global flag on tmux conf with simpler tmux env

--- scripts/tmux_basic_session.sh
+++ scripts/tmux_basic_session.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SESSION="SESSION_NAME"
-SESSION=${PWD##*/}          # to assign to a variable
-SESSION=${SESSION:-/}        # to correct for the case where PWD is / (root)
+SESSION=${PWD##*/}    # to assign to a variable
+SESSION=${SESSION:-/} # to correct for the case where PWD is / (root)
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 
 if [ "$SESSIONEXISTS" = "" ]; then
@@ -10,9 +10,6 @@ if [ "$SESSIONEXISTS" = "" ]; then
   tmux send-keys -t $SESSION:0 'nvim .' C-m
 
   tmux new-window -t $SESSION:1 -n "PRD"
-  tmux split-window -t $SESSION:1 -h
-  tmux select-pane -t $SESSION:1.0
-  tmux send-keys -t $SESSION:1.0 'ls -la' C-m
 
   tmux new-window -t $SESSION:2 -n "TST"
 
