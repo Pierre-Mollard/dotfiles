8a4add7694c1d6f6419982d542e091563166c187
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-05-11 22:06:32 +0200

    fix: added dynamic espressif conf for clangd lsp

--- stow/nvim/.config/nvim/lua/plugins/clangd.lua
+++ stow/nvim/.config/nvim/lua/plugins/clangd.lua
@@ -1,20 +1,32 @@
 return {
-  -- override nvim-lspconfig clangd only (keeps everything else) (for esp-clang)
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
     opts = {
       servers = {
         clangd = {
-          cmd = {
-            "/home/pierre/.espressif/tools/esp-clang/esp-20.1.1_20250829/esp-clang/bin/clangd",
-            "--compile-commands-dir=build",
-            "--query-driver=/home/pierre/.espressif/tools/xtensa-esp-elf/esp-15.2.0_20251204/xtensa-esp-elf/bin/xtensa-esp32-elf-gcc",
-            "--background-index",
-            "--header-insertion=iwyu",
-            "--clang-tidy",
-            "--enable-config",
-          },
+          -- Intercept the LSP initialization for clangd
+          on_new_config = function(new_config, new_root_dir)
+            -- Detect if the current workspace is an ESP-IDF project.
+            -- We do this by checking for the existence of "sdkconfig" or a main component folder.
+            local is_esp_project = vim.fn.filereadable(new_root_dir .. "/sdkconfig") == 1
+              or vim.fn.filereadable(new_root_dir .. "/sdkconfig.defaults") == 1
+
+            if is_esp_project then
+              -- Override with the custom ESP-IDF clangd and cross-compiler
+              new_config.cmd = {
+                "/home/pierre/.espressif/tools/xtensa-esp-elf/esp-15.2.0_20251204/xtensa-esp-elf/bin/xtensa-esp32-elf-gcc",
+                "--compile-commands-dir=" .. new_root_dir .. "/build",
+                "--query-driver=/home/pierre/.espressif/tools/xtensa-esp-elf/esp-15.2.0_20251204/xtensa-esp-elf/bin/xtensa-esp32-elf-gcc",
+                "--background-index",
+                "--header-insertion=iwyu",
+                "--clang-tidy",
+                "--enable-config",
+              }
+            end
+
+            -- If it's NOT an ESP project, the `cmd` is left untouched,
+            -- and LazyVim will fall back to your system's default `clangd`
+          end,
         },
       },
     },
