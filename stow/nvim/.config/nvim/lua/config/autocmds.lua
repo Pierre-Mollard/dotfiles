851af6b72ed7ccdb6e2c711a3f57ce856031de05
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-07-21 18:48:56 +0200

    conf: update nvim deps and lsp remove log file autocmd

--- stow/nvim/.config/nvim/lua/config/autocmds.lua
+++ stow/nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Automatically clear the LSP log file when Neovim exits
+vim.api.nvim_create_autocmd("VimLeavePre", {
+  desc = "Clear LSP log file on exit to prevent it from growing indefinitely",
+  callback = function()
+    local log_path = vim.fn.stdpath("state") .. "/lsp.log"
+    local f = io.open(log_path, "w")
+    if f then
+      -- Writing an empty string effectively clears the file
+      f:write("")
+      f:close()
+    end
+  end,
+})
