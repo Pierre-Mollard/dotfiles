60c4db3fd3bfe7773674acf9eaa589699b562460
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-08-14 00:35:41 +0200

    fix: make nvim plugin precognition toggle and less intrusive

--- stow/nvim/.config/nvim/lua/plugins/learning.lua
+++ stow/nvim/.config/nvim/lua/plugins/learning.lua
@@ -5,17 +5,42 @@ return {
     cmd = "VimBeGood",
   },
 
-  -- Precognition for motion hints
   {
     "tris203/precognition.nvim",
     event = "VeryLazy",
     config = function()
       require("precognition").setup({
+        startVisible = false,
+        showBlankVirtLine = false,
+        onlyVisibleLines = true,
+        maxLineLength = 200,
+
+        -- All hints must be defined (use defaults or set prio = 0 to disable)
         hints = {
-          show = true,
-          show_on_startup = false,
+          w = { text = "w", prio = 10 },
+          e = { text = "e", prio = 8 },
+          b = { text = "b", prio = 9 },
+          W = { text = "W", prio = 7 },
+          E = { text = "E", prio = 5 },
+          B = { text = "B", prio = 6 },
+          Zero = { text = "0", prio = 1 },
+          MatchingPair = { text = "%", prio = 5 },
+          Caret = { text = "^", prio = 2 },
+          Dollar = { text = "$", prio = 1 },
+        },
+
+        -- All gutter hints must be defined
+        gutterHints = {
+          G = { text = "G", prio = 10 },
+          gg = { text = "gg", prio = 9 },
+          PrevParagraph = { text = "{", prio = 8 },
+          NextParagraph = { text = "}", prio = 8 },
         },
       })
     end,
+    keys = {
+      { "<leader>cp", "<cmd>Precognition toggle<cr>", desc = "Toggle Precognition" },
+      { "<leader>cP", "<cmd>Precognition peek<cr>", desc = "Peek Precognition" },
+    },
   },
 }
