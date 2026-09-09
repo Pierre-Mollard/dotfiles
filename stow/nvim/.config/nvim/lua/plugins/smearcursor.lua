72fa5a74aefcd2bba2fe2a5c34d27f07392e5e8e
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2026-09-09 16:30:12 +0200

    fix: improve cursor behavior in lazyvim cannot change trail color

--- stow/nvim/.config/nvim/lua/plugins/smearcursor.lua
+++ stow/nvim/.config/nvim/lua/plugins/smearcursor.lua
@@ -1,26 +1,28 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-    -- Tokyonight accent colors:
-    -- You can use "#7aa2f7" (Blue - default cursor color in tokyonight)
-    -- Or "#bb9af7" (Purple), or "#c0caf5" (Main text color)
+    -- Helper Tokyonight accent colors:
+    -- #7aa2f7 : Default cursor color in tokyonight
+    -- #c0caf5 : Main text color
+    -- #bb9af7 : Purple
+    -- #3b4261 : Background
+    -- because with wezterm the actual color is the content
+    -- it is pointing at, so this will only be used for the trailing animation
     cursor_color = "#c0caf5",
 
     -- Make the smear feel snappy and fluid, not sluggish
     stiffness = 0.3,
     trailing_stiffness = 0.2,
     damping = 0.8,
-
-    -- Control how the trail looks
     distance_stop_animating = 0.05,
 
     -- Keep particles disabled for a cleaner transparent look
     particles_enabled = false,
-
     smear_between_neighbor_lines = true,
 
     -- The core hacks to make it look right over text
     never_draw_over_target = true,
     hide_target_hack = true,
+    legacy_computing_symbols_support = true,
   },
 }
